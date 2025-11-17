16a5e6b0c7adff09df352cbe7d7e9aadfe2d2370
super-gorani <june020729@gmail.com>
2025-11-17 20:30:46 +0900

    Update urls.py

--- lotto_app/urls.py
+++ lotto_app/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('buy/', views.buy),
+    path('mypage/', views.mypage),
+    path('result/<int:round_number>/', views.check_result),
+
+    # 관리자 기능
+    path('admin/draw/', views.admin_draw),
+    path('admin/sales/', views.admin_sales),
 ]
